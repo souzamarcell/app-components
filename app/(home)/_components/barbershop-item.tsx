8bfdfaad28b08b8bb6eaff508615034cf648c4c5
Marcell Souza <marcell.ramos@gmail.com>
2024-06-25 17:21:06 -0400

    feat: add depoy search Aula5

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { Button } from '@/app/_components/ui/button'
-import { Card, CardContent } from '@/app/_components/ui/card'
-import { Barbershop } from '@prisma/client'
-import { Badge } from '@/app/_components/ui/badge'
-import { StarIcon } from 'lucide-react'
-import Image from 'next/image'
+import { Badge } from "@/app/_components/ui/badge";
+import { Button } from "@/app/_components/ui/button";
+import { Card, CardContent } from "@/app/_components/ui/card";
+import { Barbershop } from "@prisma/client";
+import { StarIcon } from "lucide-react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 interface BarbershopItemProps {
-  barbershop: Barbershop
+  barbershop: Barbershop;
 }
 
 const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
@@ -19,33 +19,21 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
     router.push(`/barbershops/${barbershop.id}`);
   };
 
-
   return (
-    // <Card className="min-w-[167px] max-w-[167px]">
-    <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
-      <CardContent className="px-1 py-0">
-        <div className="h-[159px] w-full relative">
+    <Card className="min-w-full max-w-full rounded-2xl">
+      <CardContent className="px-1 py-0 pt-1">
+        <div className="w-full h-[159px] relative">
           <div className="absolute top-2 left-2 z-50">
-            <Badge
-              variant="secondary"
-              className="opacity-90 flex gap-1 items-center top-3 left-3"
-            >
-              <StarIcon
-                size={12}
-                className="fill-primary text-primary"
-              />
+            <Badge variant="secondary" className="opacity-90 flex gap-1 items-center top-3 left-3">
+              <StarIcon size={12} className="fill-primary text-primary" />
               <span className="text-xs">5,0</span>
             </Badge>
           </div>
-
           <Image
             alt={barbershop.name}
             src={barbershop.imageUrl}
-            // height={0}
-            // width={0}
-            // sizes="100vw"
             style={{
-              objectFit: 'cover',
+              objectFit: "cover",
             }}
             fill
             className="rounded-2xl"
@@ -53,19 +41,15 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
         </div>
 
         <div className="px-2 pb-3">
-          <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">
-            {barbershop.name}
-          </h2>
-          <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">
-            {barbershop.address}
-          </p>
+          <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">{barbershop.name}</h2>
+          <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barbershop.address}</p>
           <Button className="w-full mt-3" variant="secondary" onClick={handleBookingClick}>
             Reservar
           </Button>
         </div>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default BarbershopItem
+export default BarbershopItem;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,62 +1,77 @@
-import { format } from "date-fns";
-import Header from "../_components/header";
-import { ptBR } from "date-fns/locale";
-import Search from "./_components/search";
-import BookingItem from "../_components/booking-item";
-import { db } from "../_lib/prisma";
-import BarbershopItem from "./_components/barbershop-item";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../_lib/auth";
+import { format } from 'date-fns'
+import Header from '../_components/header'
+import { ptBR } from 'date-fns/locale'
+import Search from './_components/search'
+import BookingItem from '../_components/booking-item'
+import { db } from '../_lib/prisma'
+import BarbershopItem from './_components/barbershop-item'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../_lib/auth'
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions)
 
-  const [barbershops, recommendedBarbershops, confirmedBookings] = await Promise.all([
-    db.barbershop.findMany({}),
-    db.barbershop.findMany({
-      orderBy: {
-        id: "asc",
-      },
-    }),
-    session?.user
-      ? db.booking.findMany({
-          where: {
-            userId: (session.user as any).id,
-            date: {
-              gte: new Date(),
+  const [barbershops, recommendedBarbershops, confirmedBookings] =
+    await Promise.all([
+      db.barbershop.findMany({}),
+      db.barbershop.findMany({
+        orderBy: {
+          id: 'asc',
+        },
+      }),
+      session?.user
+        ? db.booking.findMany({
+            where: {
+              userId: (session.user as any).id,
+              date: {
+                gte: new Date(),
+              },
             },
-          },
-          include: {
-            service: true,
-            barbershop: true,
-          },
-        })
-      : Promise.resolve([]),
-  ]);
+            include: {
+              service: true,
+              barbershop: true,
+            },
+          })
+        : Promise.resolve([]),
+    ])
 
   return (
     <div>
       <Header />
 
-      <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold">
-          {session?.user ? `Olá, ${session.user.name?.split(" ")[0]}!` : "Olá! Vamos agendar um corte hoje?"}
-        </h2>
-        <p className="capitalize text-sm">
-          {format(new Date(), "EEEE',' dd 'de' MMMM", {
-            locale: ptBR,
-          })}
-        </p>
+      <div className="flex gap-24">
+        <div className="px-5 pt-5">
+          <h2 className="text-xl font-bold">
+            {session?.user
+              ? `Olá, ${session.user.name?.split(' ')[0]}!`
+              : 'Olá! Vamos agendar um corte hoje?'}
+          </h2>
+          <p className="capitalize text-sm">
+            {format(new Date(), "EEEE',' dd 'de' MMMM", {
+              locale: ptBR,
+            })}
+          </p>
+        </div>
+        <div className="px-5 pt-5">
+          <a href="https://www.linkedin.com/in/marcellsouza/">
+            🟣Developer
+            <img
+              alt="Marcell Souza / Linkedin"
+              src="https://img.shields.io/badge/-Marcell Souza-835afd?style=flat&logo=Linkedin&logoColor=fff"
+            />
+          </a>
+        </div>
       </div>
 
       <div className="px-5 mt-6">
         <Search />
       </div>
-
       <div className="mt-6">
         {confirmedBookings.length > 0 && (
           <>
-            <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+            <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+              Agendamentos
+            </h2>
             <div className="px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
               {confirmedBookings.map((booking) => (
                 <BookingItem key={booking.id} booking={booking} />
@@ -65,9 +80,10 @@ export default async function Home() {
           </>
         )}
       </div>
-
       <div className="mt-6">
-        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+          Recomendados
+        </h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
@@ -77,9 +93,10 @@ export default async function Home() {
           ))}
         </div>
       </div>
-
       <div className="mt-6 mb-[4.5rem]">
-        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+          Populares
+        </h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {recommendedBarbershops.map((barbershop) => (
@@ -90,5 +107,5 @@ export default async function Home() {
         </div>
       </div>
     </div>
-  );
+  )
 }
